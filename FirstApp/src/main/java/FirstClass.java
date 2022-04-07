57da2cf79956376c351b318f1619294443eb4af2
Mahamadou <mahamadou.traore@mtit-us.com>
2022-04-07 09:06:42 +0000

    Test

---
+++ FirstApp/src/main/java/FirstClass.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Admin
+ *
+ */
+public class FirstClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hi there!");
+
+	}
+
+}
---
+++ FirstApp/src/test/java/FirstClassTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FirstClassTest {
+
+	@Test
+	public void test() {
+		assertEquals("Checking size of List", 3, 3);
+	}
+
+}
